03cf709c4cb6a7fe2f3af5ba88b17dd85c90c9ee
Kaakaanuts <nayaka_bintang_pratama_rpl2023@smkn1-sby.sch.id>
2025-08-22 09:51:55 +0700

    testing

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -21,7 +21,7 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-            <h1>Selamat Datang {{ user.name }} di Chat App, selamat berinteraksi!</h1>
+            <h1>Sugeng Rawuh {{ user.name }} ing Chat App, selamat berinteraksi!</h1>
         </div>
     </AppLayout>
 </template>
