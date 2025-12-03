a9207da7efd2a37353c5fb487ee00cbb0ee821db
Kaakaanuts <nayaka_bintang_pratama_rpl2023@smkn1-sby.sch.id>
2025-12-03 11:24:32 +0700

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -21,25 +21,13 @@ export default defineConfig({
             },
         }),
     ],
-<<<<<<< HEAD
-    server: {
-        host: '0.0.0.0',
-        port: 5173, // biar bisa diakses di hp
-        strictPort: true,
-        hmr: {
-            host: '10.103.196.65', // <-- GANTI DENGAN IP ANDA
-            protocol: 'ws',
-        },
-    },  
-=======
     // server: {
     //     host: '0.0.0.0',
     //     port: 5173, // biar bisa diakses di hp
     //     strictPort: true,
     //     hmr: {
-    //         host: '10.167.90.4', // <-- GANTI DENGAN IP ANDA
+    //         host: '10.103.196.65', // <-- GANTI DENGAN IP ANDA
     //         protocol: 'ws',
     //     },
     // },  
->>>>>>> 2ad6c22 (progress setting realtime demo video call group)
 });
